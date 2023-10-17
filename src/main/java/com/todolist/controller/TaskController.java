24a04501f3c2da3eedf4ae8c2a0b4448a8c1601c
duduhrq <eduardohenriq0612002@gmail.com>
2023-10-17 09:35:10 -0300

    resolve warn

--- src/main/java/com/todolist/controller/TaskController.java
+++ src/main/java/com/todolist/controller/TaskController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.todolist.model.Tag;
 import com.todolist.model.Task;
