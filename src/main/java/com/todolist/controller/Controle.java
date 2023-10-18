38850dfd60db2028bd7f3e9e7c69ac1bd565f289
duduhrq <eduardohenriq0612002@gmail.com>
2023-10-18 12:48:33 -0300

    completed

--- src/main/java/com/todolist/controller/Controle.java
+++ src/main/java/com/todolist/controller/Controle.java
@@ -1,9 +1,12 @@
 package com.todolist.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -47,7 +50,6 @@ public class Controle {
 
     var query2 = this.tarefa.findByTag(query);
 
-
     modelo.addAttribute("tarefas", query2);
     modelo.addAttribute("categorias", query1);
     modelo.addAttribute("tag", tag);
@@ -63,7 +65,7 @@ public class Controle {
   @PostMapping("/data/create")
   public String create(@ModelAttribute Task tarefa, HttpServletRequest request, Tag tags) {
 
-    System.out.println(tarefa);
+    System.out.println("creating" + tarefa);
 
     var tagName = request.getParameter("tag_name");
 
@@ -71,7 +73,7 @@ public class Controle {
       var resultado = this.categoria.findByName(tagName);
 
       if (resultado == null) {
-        System.out.println("NOT FOUND");
+        System.out.println("TAG NOT FOUND");
         tags.setName(tagName);
         var savedTag = this.categoria.save(tags);
 
@@ -90,6 +92,50 @@ public class Controle {
 
     return "redirect:/";
 
+  }
+
+  @PostMapping("/data/delete/{id}")
+  public String delete(@PathVariable String id) {
+    var res = this.tarefa.findByTaskId(UUID.fromString(id));
+
+    if (res != null) {
+      System.out.println("FOUND");
+      this.tarefa.delete(res);
+    } else {
+      var res2 = this.categoria.findById(UUID.fromString(id));
+
+      if (res2.isPresent()) {
+
+        var findTaskWithTag = this.tarefa.findByTag(res2.get());
+
+        findTaskWithTag.forEach(e -> e.setTag(null));
+
+        this.categoria.delete(res2.get());
+      } else {
+        System.out.println("NOT FOUND");
+      }
+    }
+
+    return "redirect:/";
+
+  }
+
+  @PostMapping("/data/updateStatus/{id}")
+  public String updateStatus(@PathVariable UUID id) {
+    var query = this.tarefa.findByTaskId(id);
+
+    if (query != null) {
+      if (query.isStatus() == false) {
+        query.setStatus(true);
+      } else {
+        query.setStatus(false);
+      }
+
+      this.tarefa.save(query);
+
+    }
+
+    return "redirect:/";
   }
 
 }
--- src/main/java/com/todolist/repository/TaskRepo.java
+++ src/main/java/com/todolist/repository/TaskRepo.java
@@ -3,14 +3,17 @@ package com.todolist.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.todolist.model.Tag;
 import com.todolist.model.Task;
 import java.util.List;
 
-
 public interface TaskRepo extends JpaRepository<Task, UUID> {
-  
+
   List<Task> findByTag(Tag tag);
+
+  @Query("SELECT t FROM tb_tasks t WHERE id = ?1")
+  Task findByTaskId(UUID id);
 
 }
